0cd363b54baf92bf6e5e549620e90343063cbd74
Salah Eddin Salam <144003465+SalahSalam@users.noreply.github.com>
2025-01-31 15:36:03 +0100

    New Add: OrderHistory class. OrderHistoryRpository. Updated User class.

---
+++ OrderHistoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebShopSem4
+{
+    public class OrderRepository
+    {
+        private readonly List<OrderHistory> orderHistories = new List<OrderHistory>();
+
+        public void AddOrder(OrderHistory order)
+        {
+            orderHistories.Add(order);
+        }
+
+        public IEnumerable<OrderHistory> GetOrdersByUserId(int userId)
+        {
+            return orderHistories.Where(o => o.UserId == userId);
+        }
+    }
+}
--- User.cs
+++ User.cs
@@ -14,6 +14,8 @@ namespace WebShopSem4
         public string? Username { get; set; }
         public Role UserRole { get; set; }
         public ShoppingCart Cart { get; private set; } = new ShoppingCart();
+        private OrderRepository OrderRepository = new OrderRepository();
+
         public override string ToString()
         {
             return $"Id: {Id}, Username: {Username}, Role: {UserRole}";
