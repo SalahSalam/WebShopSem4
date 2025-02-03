bee60f1e4de3d02124650cf4d17a5eca5a19ab7f
Salah Eddin Salam <144003465+SalahSalam@users.noreply.github.com>
2025-02-03 14:13:17 +0100

    NewFeature: PasswordPolicy.Userstory1+2

--- User.cs
+++ User.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace WebShopSem4
 {
@@ -13,12 +14,37 @@ namespace WebShopSem4
         public int Id { get; set; }
         public string? Username { get; set; }
         public Role UserRole { get; set; }
+        public string? Password { get; set; }
         public ShoppingCart Cart { get; private set; } = new ShoppingCart();
         private OrderRepository OrderRepository = new OrderRepository();
 
         public override string ToString()
         {
             return $"Id: {Id}, Username: {Username}, Role: {UserRole}";
+        }
+        private void ValidatePasswords()
+        {
+            if (Password == null)
+                throw new ArgumentNullException("Password cannot be null");
+
+            if (Password.Length < 12 || Password.Length > 64)
+                throw new ArgumentOutOfRangeException("Password must be at least 12 and no more than 64 characters long");
+        }
+        private void ValidatePasswordComplexity()
+        {
+            if (!Regex.IsMatch(Password, @"[A-Z]"))
+                throw new ArgumentException("Password must contain at least one uppercase letter");
+            if (!Regex.IsMatch(Password, @"[a-z]"))
+                throw new ArgumentException("Password must contain at least one lowercase letter");
+            if (!Regex.IsMatch(Password, @"[0-9]"))
+                throw new ArgumentException("Password must contain at least one digit");
+            if (!Regex.IsMatch(Password, @"[\W_]"))
+                throw new ArgumentException("Password must contain at least one special character");
+        }
+        public void ValidatePassword()
+        {
+            ValidatePasswords();
+            ValidatePasswordComplexity();
         }
     }
 }
