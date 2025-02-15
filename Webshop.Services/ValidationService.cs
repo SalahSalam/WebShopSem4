b103e9bf456d7760c141d90cc06ba4c2d1dd990a
Jesper Højlund <github@hillgrove.dk>
2025-02-15 22:50:31 +0100

    chore: add comments to validation methods about possible redundancy

--- Webshop.Services/ValidationService.cs
+++ Webshop.Services/ValidationService.cs
@@ -4,12 +4,14 @@ namespace Webshop.Services
 {
     public class ValidationService
     {
+        // TODO: Rendundant due to modelstate validatio in #UsersController. Remove or keep for redundancy?
         public bool IsEmailValid(string email)
         {
             string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
             return Regex.IsMatch(email, pattern);
         }
 
+        // TODO: Rendundant due to modelstate validatio in #UsersController. Remove or keep for redundancy?
         public bool IsPasswordValidLength(string password)
         {
             return password.Length >= 8 &&
